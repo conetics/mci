c0a9d49e7eec2a97539cd982ac73fcc36d794136
0x15BA88FF <86390213+0x15BA88FF@users.noreply.github.com>
2026-01-19 23:27:31 +0000

    feat: add specs service
    Adds a service to retrieve specs from the database.

--- src/services/specs.rs
+++ src/services/specs.rs
@@ -1,9 +1,38 @@
 use crate::{db::DbConnection, models::Spec, schema::specs};
 use diesel::prelude::*;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Default)]
+pub struct SpecFilter {
+    pub query: Option<String>,
+    pub enabled: Option<bool>,
+    pub spec_type: Option<String>,
+}
 
 pub fn get_spec(conn: &mut DbConnection, spec_id: &str) -> QueryResult<Spec> {
     specs::table
         .find(spec_id)
         .select(Spec::as_select())
         .first(conn)
+}
+
+pub fn list_specs(conn: &mut DbConnection, filter: SpecFilter) -> QueryResult<Vec<Spec>> {
+    let mut db_query = specs::table.into_boxed();
+
+    if let Some(search_term) = filter.query {
+        let pattern = format!("%{}%", search_term);
+        db_query = db_query.filter(
+            specs::id
+                .ilike(pattern.clone())
+                .or(specs::description.ilike(pattern)),
+        );
+    }
+    if let Some(enabled) = filter.enabled {
+        db_query = db_query.filter(specs::enabled.eq(enabled));
+    }
+    if let Some(stype) = filter.spec_type {
+        db_query = db_query.filter(specs::spec_type.eq(stype));
+    }
+
+    db_query.select(Spec::as_select()).load(conn)
 }
