8153592de6f5dcc1dd0f393c19c2dd40bd541346
0x15BA88FF <86390213+0x15BA88FF@users.noreply.github.com>
2026-02-10 04:36:13 -0800

    chore: remove upload and rename create_s3_client
    temporarily remove `upload_stream` and rename `create_s3_client` to
    `create_client`

--- src/s3.rs
+++ src/s3.rs
@@ -1,11 +1,10 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{
     config::{Credentials, Region},
-    primitives::ByteStream,
     Client,
 };
 
-pub async fn create_s3_client(s3_url: &str, s3_access_key: &str, s3_secret_key: &str) -> Client {
+pub async fn create_client(s3_url: &str, s3_access_key: &str, s3_secret_key: &str) -> Client {
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
     let config = aws_config::from_env().region(region_provider).load().await;
     let s3_config = aws_sdk_s3::config::Builder::from(&config)
@@ -22,21 +21,4 @@ pub async fn create_s3_client(s3_url: &str, s3_access_key: &str, s3_secret_key: 
         .build();
 
     Client::from_conf(s3_config)
-}
-
-pub async fn upload_stream(
-    client: &Client,
-    bucket: &str,
-    key: &str,
-    body: ByteStream,
-) -> Result<(), Box<dyn std::error::Error>> {
-    client
-        .put_object()
-        .bucket(bucket)
-        .key(key)
-        .body(body)
-        .send()
-        .await?;
-
-    Ok(())
 }
